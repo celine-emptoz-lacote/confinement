576f5de4fcbec1d12fa3753cbb64c8c2b125d21b
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-04-22 18:17:29 +0200

    creation de la page connexion

--- connexion.php
+++ connexion.php
@@ -18,7 +18,29 @@
         </nav>
     </header>
     <main>
-        
+        <h1>Mon compte</h1>
+    <form action="" method="POST">
+            
+            <div >
+                <input type="text" id="firstname" name="" placeholder="Votre prénom" required>
+                <input type="text" id="name" name="" placeholder="Votre nom" required>
+            </div>
+            <div> 
+                <input type="email" id="email" name="" placeholder="Votre email" required>
+                <input type="email" id="confirmation_mail" placeholder="Confirmer votre email" name="" required>
+            </div>
+            <input type="text" id="address" name="" placeholder="Votre adresse" required>
+                
+            <div>
+                <input type="number" id="code_postal" name="" placeholder="Votre code postal" required pattern="/^(([0-8][0-9])|(9[0-5]))[0-9]{3}$/">
+                <input type="text" id="ville" name="" placeholder="Votre ville" required>
+            </div>
+    
+            <input type="tel" id="telephone" name="" placeholder="06XXXXXXXX">
+            
+
+            <input type="submit" value="Modifier">
+        </form>
     </main>
 
     <footer>
