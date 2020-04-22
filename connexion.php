d8eab59f8eb9f6565dc265db8e1ffdfe7df56923
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-04-22 17:43:11 +0200

    Modification des chemin du css et image

---
+++ connexion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Connexion</title>
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <a href="#">Accueil</a>
+            <a href="#">Profil</a>
+            <a href="#">Commerce</a>
+            <a href="#">Panier</a>
+            <a href="#">Se déconnecter</a>
+        </nav>
+    </header>
+    <main>
+        
+    </main>
+
+    <footer>
+        <nav class="nav_footer">
+            <a href="#">Accueil</a>
+            <a href="#">Profil</a>
+            <a href="#">Commerces</a>
+            <a href="#">Panier</a>
+            <a href="#">Admin</a>
+            <a href="#">Se déconnecter</a>
+        </nav>
+    </footer>
+</body>
+</html>
