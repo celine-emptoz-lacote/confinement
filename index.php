cd27ac2c1ce82d7dcffbb4067b98824eafb8e89d
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-04-22 15:51:50 +0200

    Réalisation page accueil

--- index.php
+++ index.php
@@ -17,6 +17,18 @@
             <a href="#">Se déconnecter</a>
         </nav>
     </header>
+    <main>
+        <section id="first-section"> 
+            <section id="connect">
+                <h1>Bienvenue sur </h1>
+                <h1 id="shop-name"> Marseille'shop</h1>
+                <a href="# ">Se connecter</a>
+            </section>
+            <section id="image-shop">
+                <img src="https://picsum.photos/300/300" alt="Photo illustration">
+            </section>
+        </section>
+    </main>
     
 </body>
 </html>
