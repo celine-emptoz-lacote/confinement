6793e14c59e35ac8690f54aa139f402d0d1d64d5
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-04-23 11:23:31 +0200

    fin de la page inscription

--- inscription.php
+++ inscription.php
@@ -6,7 +6,7 @@
     <title>Inscription particuliers</title>
     <link href="css/styles.css" rel="stylesheet">
 </head>
-<body>
+<body class="inscription-body">
     <header>
         <nav>
             <a href="#">Accueil</a>
@@ -16,33 +16,40 @@
             <a href="#">Se déconnecter</a>
         </nav>
     </header>
-    <main>
-        <h1>Inscription pour particulier</h1>
-        <form action="" method="POST">
-            <div>
-                <input type="text" name="" placeholder="Votre speudo" required>
-                <input type="password" name="" placeholder="Votre mot de passe" size = 15 pattern=".{6,}" title="Le mot de passe doit contenir au moins 6 caractères, 1 lettre, 1 chiffre, et 1 caractère spécial." required>
-            </div>
-            <div >
-                <input type="text"  name="" placeholder="Votre prénom" required>
-                <input type="text"  name="" placeholder="Votre nom" required>
-            </div>
-            <div> 
-                <input type="email"  name="" placeholder="Votre email" required>
-                <input type="email"  placeholder="Confirmer votre email" name="" required>
-            </div>
-            <input type="text"  name="" placeholder="Votre adresse" required>
+    <main class="inscription-main">
+        <div class="inscription-main-div">
+            <h1>S'inscrire</h1>
+            <form action="" method="POST">
                 
-            <div>
-                <input type="number"  name="" placeholder="Votre code postal" required pattern="/^(([0-8][0-9])|(9[0-5]))[0-9]{3}$/">
-                <input type="text"  name="" placeholder="Votre ville" required>
-            </div>
-    
-            <input type="tel"  name="" placeholder="06XXXXXXXX">
-            
+                <div >
+                    <input type="text"  name="" placeholder=" Votre prénom" required>
+                    <input type="text"  name="" placeholder=" Votre nom" required>
+                </div>
+                
+                <div> 
+                    <input type="email"  name="" placeholder=" Votre email" required>
+                    <input type="tel"  name="" placeholder=" 06XXXXXXXX">
+                </div>
 
-            <input type="submit">
-        </form>
+                <div>
+                    <input type="password" name="" placeholder=" Votre mot de passe" size = 15 pattern=".{6,}" title="Le mot de passe doit contenir au moins 6 caractères, 1 lettre, 1 chiffre, et 1 caractère spécial." required>
+                    <input type="password" name="" placeholder=" Confirmer votre mot de passe" size = 15 pattern=".{6,}" title="Le mot de passe doit contenir au moins 6 caractères, 1 lettre, 1 chiffre, et 1 caractère spécial." required>
+                </div>
+
+                <input type="text"  name="" placeholder=" Votre adresse" required>
+                    
+                <div>
+                    <input type="number"  name="" placeholder=" Votre code postal" required pattern="/^(([0-8][0-9])|(9[0-5]))[0-9]{3}$/">
+                    <input type="text"  name="" placeholder=" Votre ville" required>
+                </div>
+        
+                
+                
+
+                <input type="submit">
+            </form>
+        </div>
+        <img src="src/images/marseilleshop.png" alt="logo">
     </main>
     <footer>
         <nav class="nav_footer">
