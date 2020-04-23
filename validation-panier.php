1ef859d431859e38acfad36e58a752d1262bdf15
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-04-23 14:52:18 +0200

    Réalisation page validation-panier.php

---
+++ validation-panier.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+			<nav>
+				<a href="#">Accueil</a>
+				<a href="#">Profil</a>
+				<a href="#">Commerce</a>
+				<a href="#">Panier</a>
+				<a href="#">Admin</a>
+				<a href="#">Se déconnecter</a>
+			</nav>
+    </header>
+    <main class="main-validation">
+        <h1>Validation</h1>
+        <section class="validation-panier">
+        <div class="validation-title2">
+                <h1>Panier</h1>
+            </div>
+            <div class="validation-image">
+                <img src="src/images/viandes/boeuf1.JPG" alt="Photo produit">
+            </div>
+            <div class="validation-description">
+                <h2>Description</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, consequatur voluptatum aliquid mollitia nulla, sit dicta dolorum, alias in doloremque eos optio facilis maxime ex quo porro voluptatem hic inventore?</p>
+            </div>
+            <p>30€</p>
+
+        </section>
+        <section class="validation-address">
+            <div class="validation-title">
+                <h1>Adresse de livraison</h1>
+            </div>
+            <div class="customer-address">
+                <ul>
+                    <li>Monsieur/Madame</li>
+                    <li>Rue de lorem</li>
+                    <li>MARSEILLE, 13000</li>
+                    <li><a href="#">Ajouter des instructions de livraison</a></li>
+                </ul>
+            </div>
+
+        </section>
+        <section class="validation-pay">
+            <div class="validation-title">
+                <h1>Mode de paiement</h1>
+            </div>
+            <div class="pay">
+                <p>VISA</p>
+                <p>Adresse de facturation: identique à l'adresse de livraison</p>
+            </div>  
+
+        </section>
+        <button class="valider" type="submit">Passer commande</button>
+
+
+
+
+    </main>
+    
+	<footer>
+		<nav class="nav_footer">
+			<a href="#">Accueil</a>
+			<a href="#">Profil</a>
+			<a href="#">Commerces</a>
+			<a href="#">Panier</a>
+			<a href="#">Admin</a>
+			<a href="#">Se déconnecter</a>
+		</nav>
+	</footer>
+    
+    
+</body>
+</html>
