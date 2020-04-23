8ebc4134bf4192c78936625bc1eeef6325c9e252
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-04-23 12:02:27 +0200

    Réalisation répertoire en cours

--- repertoire_commercant.php
+++ repertoire_commercant.php
@@ -20,9 +20,49 @@
         </nav>
     </header>
     <main>
+        <section class="first-section"> 
+            <section class="connect">
+                <h1 class="index-shop-name"> Marseille'shop</h1>
+                <h2>Boutique</h2>
+               
+            </section>
+            <section class="index-image-shop">
+                <img src="src/images/marseilleshop.png" alt="Photo illustration">
+            </section>
+            
+        </section>
+        <h1>Commerces Alimentaire</h1>
+        <section class="repertoire-shop">
+            <section class="list-shop">
+                <img scr="src/images/images-boutiques/beef-1814638_1280.webp" alt="Photo boucherie">
+                <div>
+                    <h2>Boucherie Jacques</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, temporibus neque commodi accusamus dolores corrupti quaerat recusandae, modi dolorum earum alias, corporis natus ab consectetur numquam maxime non! Inventore, ipsam!</p>
+                <div>
+            </section>
+            <section class="list-shop">
+                <img scr="src/images/images-boutiques/beef-1814638_1280.webp" alt="Photo boucherie">
+                <div>
+                    <h2>Boucherie Jacques</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, temporibus neque commodi accusamus dolores corrupti quaerat recusandae, modi dolorum earum alias, corporis natus ab consectetur numquam maxime non! Inventore, ipsam!</p>
+                <div>
+            </section>
+            <section class="list-shop">
+                <img scr="src/images/images-boutiques/beef-1814638_1280.webp" alt="Photo boucherie">
+                <div>
+                    <h2>Boucherie Jacques</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, temporibus neque commodi accusamus dolores corrupti quaerat recusandae, modi dolorum earum alias, corporis natus ab consectetur numquam maxime non! Inventore, ipsam!</p>
+                <div>
+            </section>
+            
+        </section>
 
 
-    
+
+        </section>
+
+      
+
     </main>
 
     <footer>
